b1bb4c15508857927ce02f5e0b61d13bb530a2e0
Edzeron <31419025+Edzeron@users.noreply.github.com>
2021-08-29 17:40:38 +0200

    Juste un edit

--- TDs/ex2/hello.c
+++ TDs/ex2/hello.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-
+printf("bute");
     printf("Hello World !\n");
     printf("It's a beautiful day outside,\n");
     printf("Birds are singing,\n");
     printf("Flowers are blooming,\n");
     printf("On days like this,\n");
     printf("It is good do go outside,\n");
-    printf("And see the sun.\n");
+    printf("And see the sun.\n");qzdqzdqzydguefueiuaefieafefaeaef
 
     return 0;
 }
