26d632de2bd8d408c9bbd5b41e5804756ac5c465
Servallou <77835600+Servallou@users.noreply.github.com>
2021-08-29 14:59:53 +0200

    Add files via upload

---
+++ ex2/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Hello World !\n");
+    printf("It's a beautiful day outside,\n");
+    printf("Birds are singing,\n");
+    printf("Flowers are blooming,\n");
+    printf("On days like this,\n");
+    printf("It is good do go outside,\n");
+    printf("And see the sun.\n");
+
+    return 0;
+}
